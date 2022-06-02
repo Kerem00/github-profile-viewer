3617ae0588c289e4af9f1911d0ccf83197d466a5
Kerem <52210913+Kerem00@users.noreply.github.com>
2022-06-02 18:28:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="GitHub"))
 
 @client.tree.command()
-@app_commands.describe(username="GitHub  username.")
+@app_commands.describe(username="GitHub username.")
 async def profile(interaction: discord.Interaction, username: str):
     """Show GitHub profile by username."""
     if not re.match(r"^\w+$", username):
