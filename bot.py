6101d8ab9a85e5926d8073c8c552d1e69ec8c697
Kerem <52210913+Kerem00@users.noreply.github.com>
2022-06-04 20:51:56 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -23,19 +23,20 @@ from datetime import datetime
 
 BOT_TOKEN = keyring.get_password("bot", "token")
 APP_ID = keyring.get_password("app", "id")
+headers = {"User-Agent": "GitHub Profile Viewer", "Authorization": f"token {keyring.get_password('github', 'token')}"}
 
 def search_github_user(username):
     if not re.match(r"^\w+$", username):
         return
     try:
         info = {}
-        user = requests.get(f"https://api.github.com/users/{username}")
+        user = requests.get(f"https://api.github.com/users/{username}", headers=headers)
         user.raise_for_status()
         user = user.json()
         info["user"] = user
-        info["repos"] = requests.get(user["repos_url"]).json()
-        info["followers"] = requests.get(user["followers_url"]).json()
-        info["following"] = requests.get(user["following_url"]).json()
+        info["repos"] = requests.get(user["repos_url"], headers=headers).json()
+        info["followers"] = requests.get(user["followers_url"], headers=headers).json()
+        info["following"] = requests.get(user["following_url"][0:-13], headers=headers).json()
         return info
     except requests.exceptions.HTTPError as e:
         if e.response.status_code == 404:
@@ -48,6 +49,43 @@ class Client(discord.Client):
 
     async def setup_hook(self):
         await self.tree.sync()
+
+class ViewButtons(discord.ui.View):
+    def __init__(self, default_embed, interaction_message, info):
+        super().__init__()
+        self.default_embed = default_embed
+        self.interaction_message = interaction_message
+        self.info = info
+        self.add_item(ButtonProfile())
+        self.add_item(ButtonTemplate("Repos"))
+        self.add_item(ButtonTemplate("Followers"))
+        self.add_item(ButtonTemplate("Following"))
+
+class ButtonProfile(discord.ui.Button):
+    def __init__(self):
+        super().__init__(style=discord.ButtonStyle.primary, label="Profile")
+
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        view: ViewButtons = self.view
+        await view.interaction_message.edit(embed=view.default_embed)
+        await interaction.response.defer()
+
+class ButtonTemplate(discord.ui.Button):
+    def __init__(self, btnlabel: str):
+        super().__init__(style=discord.ButtonStyle.primary, label=btnlabel)
+        self.btnlabel = btnlabel
+
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        view: ViewButtons = self.view
+        embed = discord.Embed(title=self.btnlabel, color=0x2f3136)
+        embed.set_author(name=view.info["user"]["login"], icon_url=view.info["user"]["avatar_url"], url=view.info["user"]["html_url"])
+        for i in view.info[self.btnlabel.lower()]:
+            embed.add_field(name=i["name"] if i.get("name") is not None else i["login"], value=i["description"] if i.get("description") is not None else f"[Click]({i['html_url']})", inline=False)
+        embed.set_footer(text=datetime.strptime(view.info["user"]["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime('Created on %d %b, %Y.'))
+        await view.interaction_message.edit(embed=embed)
+        await interaction.response.defer()
 
 client = Client(intents=discord.Intents.default(), application_id=APP_ID)
 
@@ -62,8 +100,9 @@ async def on_ready():
 @app_commands.describe(username="GitHub username.")
 async def profile(interaction: discord.Interaction, username: str):
     """Show GitHub profile by username."""
+    await interaction.response.defer()
     if not re.match(r"^\w+$", username):
-        await interaction.response.send_message(content="Specified username is invalid.")
+        await interaction.edit_original_message(content="Specified username is invalid.")
         return
     info = search_github_user(username)
     if info is not None:
@@ -73,8 +112,8 @@ async def profile(interaction: discord.Interaction, username: str):
         embed.add_field(name="Followers", value=str(info["user"]["followers"]))
         embed.add_field(name="Following", value=str(info["user"]["following"]))
         embed.set_footer(text=datetime.strptime(info["user"]["created_at"], "%Y-%m-%dT%H:%M:%SZ").strftime('Created on %d %b, %Y.'))
-        await interaction.response.send_message(embed=embed)
+        await interaction.edit_original_message(embed=embed, view=ViewButtons(embed, await interaction.original_message(), info))
     else:
-        await interaction.response.send_message(content=f"'{username}' not found.")
+        await interaction.edit_original_message(content=f"'{username}' not found.")
 
 client.run(BOT_TOKEN)
