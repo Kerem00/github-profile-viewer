07f5f519320766f581e3b41653d465a40de65139
Kerem <52210913+Kerem00@users.noreply.github.com>
2025-08-29 23:54:27 +0300

    Change embed color

--- bot.py
+++ bot.py
@@ -177,7 +177,7 @@ async def profile(interaction: discord.Interaction, username: str):
         return
     info = search_github_user(username)
     if info is not None:
-        embed = discord.Embed(title=info["name"], description=info["bio"], color=0x2b2d31)
+        embed = discord.Embed(title=info["name"], description=info["bio"], color=0x1a1a1e)
         embed.set_author(name=info["login"], icon_url=info["avatar_url"], url=info["html_url"])
         embed.add_field(name="Repos", value=str(info["public_repos"]))
         embed.add_field(name="Followers", value=str(info["followers"]))
